de065fb92245d60fee890044903f0502ece87aa6
hucq <chengquan.hu@gmail.com>
2018-11-29 12:46:51 +0800

    update

---
+++ src/pages/index/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div @click="clickHandle('test click', $event)">
+
+    <div class="userinfo" @click="bindViewTap">
+      <!-- <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" /> -->
+      <!-- <image class="index-logo" src="https://avatars1.githubusercontent.com/u/1715463?s=40&v=4"></image> -->
+
+<!--       <img class="userinfo-avatar" src="../../images/user.jpeg" background-size="cover" />
+      <image class="index-logo" src="../../images/user.jpeg"></image> -->
+
+      <div class="userinfo-nickname">
+        <!-- <card text="userInfo.nickName"></card> -->
+      </div>
+    </div>
+
+    <div class="usermotto">
+      <div class="user-motto">
+        <card :text="motto"></card>
+      </div>
+    </div>
+
+    <form class="form-container">
+      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
+      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
+    </form>
+
+
+    <div class="all">
+        <div class="left">
+        </div>
+        <div class="right">
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {
+        nickName: 'huchengquan',
+        avatarUrl: 'https://avatars1.githubusercontent.com/u/1715463?s=40&v=4'
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      swan.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      // swan.login({
+      //   success: () => {
+      //     wx.getUserInfo({
+      //       success: (res) => {
+      //         this.userInfo = res.userInfo
+      //       }
+      //     })
+      //   }
+      // })
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    // this.getUserInfo()
+    let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
---
+++ src/pages/logs/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './index'
+// Vue.config.errorHandler = function (err) {
+//   if (console && console.error) {
+//     console.error(err)
+//   }
+// }
+const app = new Vue(App)
+app.$mount()
